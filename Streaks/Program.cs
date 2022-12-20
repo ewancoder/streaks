70eac956cc93a07d08f894b7daf50b9541725484
Ivan Zyranau <ewancoder@gmail.com>
2022-12-21 00:25:33 +0300

    Show not done streaks first

--- Streaks/Program.cs
+++ Streaks/Program.cs
@@ -106,7 +106,18 @@ while (true)
 
     Console.WriteLine($"{"Activity",-20}{"Need to do",-15}{"In time",-50}{"Next cycle starts at"}");
     var color = ConsoleColor.DarkGray;
-    foreach (var streak in streaks.OrderBy(x => x.AbsoluteDeadLine))
+
+    foreach (var streak in streaks.Where(x => !x.DoneThisStreak).OrderBy(x => x.AbsoluteDeadLine))
+    {
+        color = color == ConsoleColor.DarkGray ? ConsoleColor.Black : ConsoleColor.DarkGray;
+        Console.BackgroundColor = color;
+
+        Console.WriteLine($"{streak.ActivityId,-20}{(streak.DoneThisStreak ? streak.NeedToDo + " (done)" : streak.NeedToDo),-15}{GetHumanTime(streak.AbsoluteDeadLine-DateTime.Now),-50}{(streak.DoneThisStreak ? streak.NextCycleStartsAt : "")}");
+    }
+
+    Console.WriteLine();
+
+    foreach (var streak in streaks.Where(x => x.DoneThisStreak).OrderBy(x => x.AbsoluteDeadLine))
     {
         color = color == ConsoleColor.DarkGray ? ConsoleColor.Black : ConsoleColor.DarkGray;
         Console.BackgroundColor = color;
